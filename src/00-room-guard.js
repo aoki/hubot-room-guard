cfdfc5d0a956e624699c2159a460cdeebd915d56
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-07-22 19:34:55 +0900

    Add scripts

---
+++ src/00-room-guard.js
@@ -0,0 +1,49 @@
+// Description:
+//   room guard and operator guard
+// Configuration:
+//   HUBOT_ROOM_GUARD_ENABLE_ROOMS []
+//   HUBOT_ROOM_GUARD_ENABLE_OPERATORS []
+
+// console.log( '✔ Load: ' + __filename.split('/').pop());
+
+var _ = require('lodash');
+
+function loadConfig(str) {
+  try {
+    if (str === '') return [];
+    else return JSON.parse(str);
+  } catch (e) {
+    return console.error(e);
+  }
+}
+
+var config = {
+  rooms: loadConfig(process.env.HUBOT_ROOM_GUARD_ENABLE_ROOMS),
+  operators: loadConfig(process.env.HUBOT_ROOM_GUARD_ENABLE_OPERATORS)
+};
+
+module.exports = function(robot) {
+  'use strict';
+
+  var operators = config.operators;
+  var rooms = config.rooms;
+
+  robot.hear(/.*/, function(msg) {
+
+    // Validate permission to operation
+    if (! _.includes(operators, msg.envelope.user.name)) {
+      return;
+    }
+
+    // Enable rooms
+    if (! _.includes(rooms, msg.envelope.room)) {
+      msg.finish();
+    }
+
+    // Ignore users by regex
+    if (/bot/.test(msg.envelope.user.name)) {
+      msg.finish();
+    }
+
+  });
+};
